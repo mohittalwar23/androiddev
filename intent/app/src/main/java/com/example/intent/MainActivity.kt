4e90b3dadab3fc3bdf1dbb4d02cb98e266924b4e
Mohit <talwarmohit2005@gmail.com>
2024-01-29 23:58:36 +0530

    added second activity

--- intent/app/src/main/java/com/example/intent/MainActivity.kt
+++ intent/app/src/main/java/com/example/intent/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var name : TextView
+    lateinit var age : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        name = findViewById(R.id.inputName)
+        age = findViewById(R.id.inputAge)
+
+        intent = Intent(this@MainActivity,Seco)
     }
 }
