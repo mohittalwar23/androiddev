abd533f56adabdd967159de950b93a225e7d9f80
Mohit <talwarmohit2005@gmail.com>
2024-02-14 23:56:40 +0530

    learnt java and some more widgets

--- NewWidgets/app/src/main/java/com/example/newwidgets/MainActivity.java
+++ NewWidgets/app/src/main/java/com/example/newwidgets/MainActivity.java
@@ -5,28 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     CheckBox checkBox;
+    RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        checkBox=findViewById(R.id.checkBox);
+        checkBox = findViewById(R.id.checkBox);
+        radioGroup = findViewById(R.id.radioGroup);
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    Toast.makeText( MainActivity.this,"checked",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "checked", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, " not checked", Toast.LENGTH_SHORT).show();
                 }
-                else {
-                    Toast.makeText( MainActivity.this," not checked",Toast.LENGTH_SHORT).show();
-                }
-                };
+            }
+
+            ;
         });
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = findViewById(checkedId);
+                Toast.makeText(MainActivity.this, "this is " + radioButton.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }}
-
-
